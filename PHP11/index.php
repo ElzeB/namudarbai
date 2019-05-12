9d2d8505bcd921d7a24f78038ad1aa833c1a7447
ElzeB <bernackyte@gmail.com>
2019-05-12 09:20:52 +0300

    Namu darbas PHP11

---
+++ PHP11/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Namų darbai PHP11</title>
+</head>
+
+<body>
+    <?php
+    function plotas($a, $b){
+        $plotas = $a*$b;
+        return $plotas;
+    }
+
+    if(isset($_POST['mygtukas'])){
+        if($_POST['ilgis'] && $_POST['plotis']){
+            echo 'Stačiakampio plotas: '.plotas($_POST['ilgis'],$_POST['plotis']);
+                }else{
+                    echo 'Įveskite visus parametrus';
+                }
+    }else { ?>
+        <form action = "<?php $_PHP_SELF; ?>"; method="post">
+            <p> Įveskite ilgį: <input type="number" name="ilgis"></p>
+            <p> Įveskite plotį: <input type="number" name="plotis"></p>
+            <input type="submit" value="Send" name="mygtukas">
+        </form>
+   <?php } ?>
+
+</body>
+
+</html>
